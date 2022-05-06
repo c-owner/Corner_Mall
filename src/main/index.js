e784421cb79f9c8361cc95fcb62af63b42a6bbe5
Eight-Corner <corner3499@gmail.com>
2022-05-07 01:49:05 +0900

    ⚙️ fix

--- src/main/index.js
+++ src/main/index.js
@@ -1,56 +1,58 @@
+import React from "react";
 import "./index.css";
 import axios from "axios";
-import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function MainPage() {
     const [products, setProducts] = React.useState([]);
-    React.useEffect(
-        function () {
-            axios.get('https://644cc84a-ae0f-48c0-a6b9-7a52be885cb2.mock.pstmn.io/products').then(function (result) {
+    React.useEffect(function () {
+        axios
+            .get(
+                "https://644cc84a-ae0f-48c0-a6b9-7a52be885cb2.mock.pstmn.io/products"
+            )
+            .then(function (result) {
                 const products = result.data.products;
                 setProducts(products);
-            }).catch(function (err) {
-                console.log(err);
+            })
+            .catch(function (error) {
+                console.error("에러 발생 : ", error);
             });
-        }, []);
-
-    if (products === null) {
-        return <h1>상품 정보를 가져오고 있습니다..</h1>
-    }
+    }, []);
 
     return (
         <div>
-            <div id="body">
-                <div id="banner">
-                    <img src="images/banners/banner1.png" alt=""/>
-                </div>
-                <h1>판매되는 상품들</h1>
-                <div id="product-list">
-                    {
-                        products.map(function (item, p_idx) {
-                            return (
-                                <div className="product-card">
-                                    <Link style={{ color: "inherit" }} className={"product-link"} to={`/product/${item.id}`}>
-                                        <div>
-                                            <img className="product-img" src={item.imageUrl} alt=""/>
-                                        </div>
-                                        <div className={"product-contents"}>
-                                            <span className="product-name">{item.name}</span>
-                                            <span className="product-price">{item.price}원</span>
-                                            <div className="product-seller">
-                                                <img className="product-avatar" src="images/icons/avatar.png" alt=""/>
-                                                <span>{item.seller}</span>
-                                            </div>
-                                        </div>
-                                    </Link>
-                                </div>
-                            );
-                        })
-                    }
-                </div>
+            <div id="banner">
+                <img src="images/banners/banner1.png" />
             </div>
-
+            <h1>판매되는 상품들</h1>
+            <div id="product-list">
+                {products.map(function (product, index) {
+                    return (
+                        <div className="product-card">
+                            <Link
+                                style={{ color: "inherit" }}
+                                className="product-link"
+                                to={`/products/${product.id}`}
+                            >
+                                <div>
+                                    <img className="product-img" src={product.imageUrl} />
+                                </div>
+                                <div className="product-contents">
+                                    <span className="product-name">{product.name}</span>
+                                    <span className="product-price">{product.price}원</span>
+                                    <div className="product-seller">
+                                        <img
+                                            className="product-avatar"
+                                            src="images/icons/avatar.png"
+                                        />
+                                        <span>{product.seller}</span>
+                                    </div>
+                                </div>
+                            </Link>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 }
--- src/product/index.js
+++ src/product/index.js
@@ -1,37 +1,42 @@
 import {useParams} from "react-router-dom";
 import axios from "axios";
-import { useEffect, useState} from "react";
+import {useEffect, useState} from "react";
+import "./index.css";
 
 function ProductPage() {
     const {id} = useParams();
     const [product, setProduct] = useState(null);
-    useEffect(
-        function () {
-            axios.get(`https://644cc84a-ae0f-48c0-a6b9-7a52be885cb2.mock.pstmn.io/products/${id}`).then(function (result) {
+    useEffect(function () {
+        axios
+            .get(
+                `https://644cc84a-ae0f-48c0-a6b9-7a52be885cb2.mock.pstmn.io/products/${id}`
+            )
+            .then(function (result) {
                 setProduct(result.data);
-            }).catch(function (err) {
-                console.log(err);
+            })
+            .catch(function (error) {
+                console.error(error);
             });
-        },[]);
+    }, []);
 
     if (product === null) {
-        return <h1>상품 정보를 받고 있습니다...</h1>
+        return <h1>상품 정보를 받고 있습니다...</h1>;
     }
 
     return (
         <div>
             <div id="image-box">
-                <img src={`/${product.imageUrl}`} alt="" />
+                <img src={"/" + product.imageUrl}/>
             </div>
             <div id="profile-box">
-                <img src="/images/icons/avatar.png" alt="" />
+                <img src="/images/icons/avatar.png"/>
                 <span>{product.seller}</span>
             </div>
-            <div id="content-box">
+            <div id="contents-box">
                 <div id="name">{product.name}</div>
                 <div id="price">{product.price}원</div>
-                <div id="createdAt">2022년 5월 6일</div>
-                <div id="description">{product.description}</div>
+                <div id="createdAt">2020년 12월 8일</div>
+                <div id="description">{product.description} </div>
             </div>
         </div>
     );
