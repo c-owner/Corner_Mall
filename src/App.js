be6998201aec2628f2353f58327d428d23ddc115
Eight-Corner <corner3499@gmail.com>
2022-05-06 16:38:18 +0900

    ⚙️

--- src/App.js
+++ src/App.js
@@ -1,46 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-import ChildComponent from './child.js';
-import TimerComponent from "./timer";
-
-function SayHelloComponent() {
-    const text = "안녕 다마고치야";
-    return (
-        <div>
-            <h3>그랩이 이야기 합니다 : {text}</h3>
-        </div>
-    )
-}
+import MainPageComponent from "./main";
 
 function App() {
-    const text = '개발자 화이팅!';
-
-    const sayHello = function () {
-        return (
-            <div>
-                <h3>코너 : 리액트를 배워보자~~~</h3>
-            </div>
-        )
-    }
-    const sayAlert = function () {
-        alert('코너의 Alert 창 ');
-    }
-    return (
-        <div>
-            <img src={logo} width={300}/>
-            <h1>안녕하세요!</h1>
-            <h2>{text}</h2>
-            {sayHello()}
-            <div onClick={sayAlert}>
-                <button>클릭해봐</button>
-            </div>
-            <ChildComponent name={"코너"} age={28} />
-            <ChildComponent name={"김치"} age={24} />
-            <ChildComponent name={"이유"} age={30} />
-            <TimerComponent />
-            <SayHelloComponent />
-        </div>
-    );
+    return <MainPageComponent />;
 }
 
 export default App;
---
+++ src/main/index.js
@@ -0,0 +1,27 @@
+import "./index.css";
+
+function MainPage() {
+    return (
+        <div>
+            <div id="header">
+                <div id="header-area">
+                    <img src="images/icons/logo.png"/>
+                </div>
+            </div>
+            <div id="body">
+                <div id="banner">
+                    <img src="images/banners/banner1.png"/>
+                </div>
+                <h1>판매되는 상품들</h1>
+                <div id="product-list">
+
+                </div>
+                <div id="footer">
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MainPage;
