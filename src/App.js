8dbd577ce642f54e26d6e03e3a9205d1a19aab8e
Eight-Corner <corner3499@gmail.com>
2022-05-08 03:24:15 +0900

    react- router pathname

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "antd/dist/antd.min.css";
 import "./App.css";
 import MainPageComponent from "./main";
-import { Switch, Route, Link, useHistory } from "react-router-dom";
+import {Switch, Route, Link, useHistory, useLocation} from "react-router-dom";
 import UploadPage from "./upload";
 import ProductPage from "./product";
-import { Button } from "antd";
-import { DownloadOutlined } from "@ant-design/icons";
+import {Button} from "antd";
+import {DownloadOutlined} from "@ant-design/icons";
+import {useEffect} from "react";
+
 function App() {
     const history = useHistory();
+    let location = useLocation();
     return (
         <div>
             <div id="header">
@@ -15,27 +18,31 @@ function App() {
                     <Link to="/">
                         <img src="/images/icons/logo.png" alt=""/>
                     </Link>
-                    <Button
-                        size="large"
-                        onClick={function () {
-                            history.push("/upload");
-                        }}
-                        icon={<DownloadOutlined />}
-                    >
-                        상품 업로드
-                    </Button>
+                    {
+                        location.pathname === '/upload' ?
+                            <span></span> :
+                            <Button
+                                size="large"
+                                onClick={function () {
+                                    history.push("/upload");
+                                }}
+                                icon={<DownloadOutlined/>}
+                            >
+                                상품 업로드
+                            </Button>
+                    }
                 </div>
             </div>
             <div id="body">
                 <Switch>
                     <Route exact={true} path="/">
-                        <MainPageComponent />
+                        <MainPageComponent/>
                     </Route>
                     <Route exact={true} path="/products/:id">
-                        <ProductPage />
+                        <ProductPage/>
                     </Route>
                     <Route exact={true} path="/upload">
-                        <UploadPage />
+                        <UploadPage/>
                     </Route>
                 </Switch>
             </div>
