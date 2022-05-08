f6b6c55ca0f95c185bb8bbc1e0ebf27ada7dcf75
Eight-Corner <corner3499@gmail.com>
2022-05-08 17:59:31 +0900

    apiURL update

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,5 @@
-export const API_URL = 'http://corner-mac.kro.kr:8080';
+export const API_URL =
+    process.env.NODE_ENV === 'production' ? 'https://corner-mall-server.herokuapp.com' : 'http://localhost:8080';
+// export const API_URL = 'http://corner-mac.kro.kr:8080';
 // export const API_URL = 'http://localhost:8080';
 
